a031457868ff9d9e4ec27bf2811a2e7fcecb3312
itsmhercy <mercyakinwumi36@gmail.com>
2022-03-21 16:21:06 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int index;
-	
+
 	index = 0;
 
 	while (index >= 0)
 	{
 		dest[index] = src[index];
-		
+
 		if (src[index] == '\0')	
 		{
 			return (dest);
