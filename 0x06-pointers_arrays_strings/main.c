eee4182c405e48eb7c08c640f5cff12d2c6d2fcc
itsMhercy <mercyakinwumi36@gmail.com>
2022-03-24 13:01:27 +0100

    main.h

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 /**
  *
- *  * main - check the code
+ * main - check the code
  *
- *   *
  *
- *    * Return: Always 0.
  *
- *     */
+ * Return: Always 0.
+ *
+ */
 
 int main(void)
 
